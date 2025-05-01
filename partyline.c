56f7fc3373052e153f5be5c6d2819091d5658ece
Michael Harris <mharris@definite.app>
2025-05-01 16:55:16 -0600

    feat(partyline): support move/delete word and line

--- partyline.c
+++ partyline.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <signal.h>
 #include <stddef.h>
@@ -10,6 +11,8 @@
 #include "partyline.h"
 
 #define ESC 0x1b
+#define EOT 0x04
+#define DEL 0x7f
 
 static struct termios orig;
 
@@ -37,6 +40,7 @@ static int enable_raw_mode(int fd) {
 enum {
   KEY_NULL      = 0,
   KEY_CTRL_C    = 3,
+  KEY_CTRL_U    = 21,
   KEY_BACKSPACE = 127,
   ARROW_LEFT    = 1000,
   ARROW_RIGHT,
@@ -48,6 +52,9 @@ enum {
   KEY_PAGE_UP,
   KEY_PAGE_DOWN,
   KEY_EOF,
+  KEY_WORD_LEFT,
+  KEY_WORD_RIGHT,
+  KEY_DEL_WORD,
 };
 
 static int read_keypress(int fd) {
@@ -56,18 +63,36 @@ static int read_keypress(int fd) {
 
   n = read(fd, &c, 1);
 
-  if (n == 0) return KEY_EOF; // real EOF
-  if (c == 4) return KEY_EOF; // ctrl + d
+  if (n == 0) return KEY_EOF;         // real EOF
+  if (c == EOT) return KEY_EOF;       // ctrl + d
+  if (c == DEL) return KEY_BACKSPACE; // Delete key
+
   if (n == -1 && errno != EAGAIN) exit_with_error("Failed to read input");
   if (c != ESC) return c;
 
-  char seq[3];
-  if (read(fd, &seq[0], 1) != 1) return ESC;
+  char next;
+  if (read(fd, &next, 1) == 1 && (unsigned char)next == DEL) return KEY_DEL_WORD;
+
+  char seq[5];
+  seq[0] = next;
   if (read(fd, &seq[1], 1) != 1) return ESC;
 
   if (seq[0] == '[' || seq[0] == 'O') {
-    if (seq[1] >= '0' && seq[1] <= '9') {
-      if (read(fd, &seq[2], 1) != 1) return ESC;
+    if (read(fd, &seq[2], 1) != 1) return ESC;
+    if (seq[2] == ';') {
+      if (read(fd, &seq[3], 1) != 1) return ESC;
+      if (read(fd, &seq[4], 1) != 1) return ESC;
+      int  mod = seq[3] - '0';
+      char k   = seq[4];
+      if (k == 'C') {
+        if (mod == 3) return KEY_WORD_RIGHT;
+        if (mod == 5) return KEY_END;
+      }
+      if (k == 'D') {
+        if (mod == 3) return KEY_WORD_LEFT;
+        if (mod == 5) return KEY_HOME;
+      }
+    } else if (seq[1] >= '0' && seq[1] <= '9') {
       switch (seq[1]) {
         case '1': return KEY_HOME;
         case '3': return KEY_DELETE;
@@ -129,7 +154,6 @@ char *partyline(const char *prompt) {
       teardown();
       return NULL;
     } else if (key >= ' ' && key <= '~') {
-      // TODO: better dynamic buffer implementation to avoid realloc in hot loop
       buf = realloc(buf, buflen + 2);
       memmove(buf + pos + 1, buf + pos, buflen - pos);
       buf[pos] = key;
@@ -138,8 +162,7 @@ char *partyline(const char *prompt) {
       size_t suffix = buflen - pos;
       write(STDOUT_FILENO, buf + pos, suffix);
 
-      for (size_t i = 0; i < suffix - 1; i++)
-        write(STDOUT_FILENO, "\b", 1);
+      for (size_t i = 0; i < suffix - 1; i++) write(STDOUT_FILENO, "\b", 1);
 
       pos++;
     } else if (key == KEY_BACKSPACE && pos > 0) {
@@ -153,8 +176,7 @@ char *partyline(const char *prompt) {
       write(STDOUT_FILENO, buf + pos, tail);
       write(STDOUT_FILENO, " ", 1);
 
-      for (size_t i = 0; i < tail + 1; i++)
-        write(STDOUT_FILENO, "\b", 1);
+      for (size_t i = 0; i < tail + 1; i++) write(STDOUT_FILENO, "\b", 1);
     } else if (key == KEY_DELETE && pos < buflen) {
       memmove(buf + pos, buf + pos + 1, buflen - pos - 1);
       buflen--;
@@ -163,8 +185,7 @@ char *partyline(const char *prompt) {
       write(STDOUT_FILENO, buf + pos, tail);
       write(STDOUT_FILENO, " ", 1);
 
-      for (size_t i = 0; i < tail + 1; i++)
-        write(STDOUT_FILENO, "\b", 1);
+      for (size_t i = 0; i < tail + 1; i++) write(STDOUT_FILENO, "\b", 1);
     } else if (key == ARROW_LEFT && pos > 0) {
       write(STDOUT_FILENO, "\b", 1);
       pos--;
@@ -180,6 +201,37 @@ char *partyline(const char *prompt) {
       size_t tail = buflen - pos;
       write(STDOUT_FILENO, buf + pos, tail);
       pos = buflen;
+    } else if (key == KEY_WORD_LEFT) {
+      size_t orig_pos = pos;
+      while (pos > 0 && isspace(buf[pos - 1])) pos--;
+      while (pos > 0 && !isspace(buf[pos - 1])) pos--;
+      for (size_t i = 0; i < orig_pos - pos; i++) write(STDOUT_FILENO, "\b", 1);
+    } else if (key == KEY_WORD_RIGHT) {
+      size_t orig_pos = pos;
+      while (pos < buflen && isspace(buf[pos])) pos++;
+      while (pos < buflen && !isspace(buf[pos])) pos++;
+      write(STDOUT_FILENO, buf + orig_pos, pos - orig_pos);
+    } else if (key == KEY_DEL_WORD) {
+      size_t orig_pos = pos;
+      while (pos > 0 && isspace((unsigned char)buf[pos - 1])) pos--;
+      while (pos > 0 && !isspace((unsigned char)buf[pos - 1])) pos--;
+
+      size_t removed = orig_pos - pos;
+      memmove(buf + pos, buf + orig_pos, buflen - orig_pos);
+      buflen -= removed;
+
+      for (size_t i = 0; i < removed; i++) write(STDOUT_FILENO, "\b", 1);
+
+      size_t tail = buflen - pos;
+      write(STDOUT_FILENO, buf + pos, tail);
+
+      for (size_t i = 0; i < removed; i++) write(STDOUT_FILENO, " ", 1);
+      for (size_t i = 0; i < tail + removed; i++) write(STDOUT_FILENO, "\b", 1);
+    } else if (key == KEY_CTRL_U) {
+      for (size_t i = 0; i < pos; i++) write(STDOUT_FILENO, "\b", 1);
+      for (size_t i = 0; i < buflen; i++) write(STDOUT_FILENO, " ", 1);
+      for (size_t i = 0; i < buflen; i++) write(STDOUT_FILENO, "\b", 1);
+      buflen = pos = 0;
     }
   }
 
